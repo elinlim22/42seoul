f60c55af97f03cd77340a999dc2531524a91ae4a
hyeslim <hyeslim@c2r2s2.42seoul.kr>
2022-07-06 15:48:29 +0900

    first commit

---
+++ circle_1/libft/ft_memmove.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 15:41:25 by hyeslim           #+#    #+#             */
+/*   Updated: 2022/07/06 15:48:15 by hyeslim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t len)
+{
+	unsigned char	*temp;
+	size_t			i;
+
+	i = 0;
+	while (src[i])
+	{
+		temp[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	i = 0;
+	while (i < len)
+	{
+		((unsigned char *)dest)[i] = temp[i];
+		i++;
+	}
+	return (dest);
+}
