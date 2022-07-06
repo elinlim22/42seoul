fe5c3b7c369cfd7d5f66af5e70c9bf05add934fc
hyeslim <hyeslim@c2r2s2.42seoul.kr>
2022-07-06 15:20:48 +0900

    str deleted

--- circle_1/libft/ft_memset.c
+++ circle_1/libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 11:23:20 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/06 14:09:53 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/06 15:20:00 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,12 @@ void	*ft_memset(void *ptr, int value, size_t num)
 {
 	size_t			i;
 	char			c;
-	unsigned char	*str;
 
 	i = 0;
 	c = (unsigned char)value;
-	str = (unsigned char *)ptr;
 	while (i < num)
 	{
-		str[i] = c;
+		((unsigned char *)ptr)[i] = c;
 		i++;
 	}
 	return (ptr);
