7d9fffb650e4f44be8fa68f3b1d8262e60c43d13
hyeslim <hyeslim@c2r2s2.42seoul.kr>
2022-07-06 17:38:57 +0900

    no sizeof

--- circle_1/libft/ft_strlcpy.c
+++ circle_1/libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:48:39 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/06 15:51:40 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/06 17:27:11 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	size_t	i;
 
 	i = 0;
-	while (i + sizeof(size_t) < size && src[i] != '\0')
+	while (i + 1 < size && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
