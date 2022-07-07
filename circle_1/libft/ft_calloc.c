2472acca82e12e01c3fbf670a1391f7bf45e2f98
hyeslim <hyeslim@c2r6s4.42seoul.kr>
2022-07-07 19:27:30 +0900

    nochange

--- circle_1/libft/ft_calloc.c
+++ circle_1/libft/ft_calloc.c
@@ -6,19 +6,19 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:28:14 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/07 16:33:29 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/07 19:09:39 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include "ft_memset.c"
+
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*dest;
 	size_t	i;
 
 	dest = malloc(size * count);
-	if (*dest == 0)
+	if (*((char *)dest) == 0)
 		return (0);
 	i = 0;
 	ft_memset(dest, 0, count);
