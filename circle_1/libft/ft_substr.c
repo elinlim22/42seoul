6fe50c0b570fe267671ab51c8f2f4c3662ba4116
hyeslim <hyeslim@c2r1s5.42seoul.kr>
2022-07-08 12:56:06 +0900

    null guard fixed

--- circle_1/libft/ft_substr.c
+++ circle_1/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 16:31:58 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/07 19:25:01 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/08 12:54:55 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*dest;
 	int		i;
 
-	dest = (char *)malloc(sizeof(char) * len);
-	// if (*dest == 0)
-	// 	return (0);
 	i = 0;
+	dest = (char *)malloc(sizeof(char) * len);
+	if (!dest)
+		return (0);
 	while (i + 1 < (int)len)
 	{
 		dest[i] = s[start + i];
-		// dest++;
 		i++;
 	}
 	dest[i] = '\0';
