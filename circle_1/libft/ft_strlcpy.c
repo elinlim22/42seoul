481f020aa07b2c41cd4e529b37e473ecfb160a03
elinlim22 <hyeslim@c2r1s4.42seoul.kr>
2022-07-18 21:48:16 +0900

    strlcpy done

--- circle_1/libft/ft_strlcpy.c
+++ circle_1/libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:48:39 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/11 18:13:58 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/18 21:45:08 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 
 {
 	size_t	i;
+	size_t	len;
 
 	i = 0;
+	len = ft_strlen(src);
+	if (!size)
+		return (len);
 	while (i + 1 < size && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+	return (len);
 }
