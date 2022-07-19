cfb9987f1517dee2a5d98cad901726ed06574228
elinlim22 <hyeslim@c2r1s4.42seoul.kr>
2022-07-19 21:04:44 +0900

    memmove done

--- circle_1/libft/ft_memmove.c
+++ circle_1/libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:41:25 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/18 14:25:59 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/19 19:51:28 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,22 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	unsigned char	*temp;
+	unsigned char	*d;
+	unsigned char	*s;
 	size_t			i;
 
-	i = 0;
-	temp = NULL;
-	while (((unsigned char *)src)[i])
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	i = -1;
+	if (dest < src)
 	{
-		temp[i] = ((unsigned char *)src)[i];
-		i++;
+		while (++i < len)
+			d[i] = s[i];
 	}
-	i = 0;
-	while (i < len)
+	else
 	{
-		((unsigned char *)dest)[i] = temp[i];
-		i++;
+		while (++i < len)
+			d[ft_strlen(dest) - i - 1] = s[ft_strlen(src) - i - 1];
 	}
 	return (dest);
 }
