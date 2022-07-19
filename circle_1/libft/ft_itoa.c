694daa4896e9a0a1362ea34ce668dd949b0ed21e
elinlim22 <hyeslim@c2r1s4.42seoul.kr>
2022-07-19 21:04:26 +0900

    itoa done

--- circle_1/libft/ft_itoa.c
+++ circle_1/libft/ft_itoa.c
@@ -6,22 +6,28 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 14:15:50 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/18 14:17:47 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/19 20:34:26 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_len(int n)
+static int	count_len(int n, int sign)
 {
-	int	len;
+	int			len;
+	long long	temp;
 
 	len = 0;
 	if (n == 0)
 		return (1);
-	while (n > 0)
+	if (n < 0)
 	{
-		n /= 10;
+		len++;
+	}
+	temp = (long long)n * sign;
+	while (temp > 0)
+	{
+		temp /= 10;
 		len++;
 	}
 	return (len);
@@ -29,32 +35,26 @@ static int	count_len(int n)
 
 char	*ft_itoa(int n)
 {
-	int				len;
-	int				sign;
-	char			*str;
-	unsigned int	temp;
+	int		len;
+	int		sign;
+	char	*str;
 
-	sign = 0;
-	len = count_len(n);
+	sign = 1;
 	if (n < 0)
-	{
-		len++;
-		sign = 1;
-		temp = -n;
-	}
-	else
-		temp = n;
-	str = (char *)malloc(sizeof(char) * (len + sign + 1));
+		sign *= -1;
+	len = count_len(n, sign);
+	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
 	str[len] = '\0';
-	if (n < 0)
-		str[0] = '-';
-	while (len - 1 > sign - 1)
+	if (n == 0)
+		str[0] = '0';
+	while (--len >= 0 && n != 0)
 	{
-		str[len - 1] = (temp % 10 + '0');
-		len--;
-		temp /= 10;
+		str[len] = (n % 10) * sign + '0';
+		n /= 10;
 	}
+	if (sign == -1)
+		str[0] = '-';
 	return (str);
 }
