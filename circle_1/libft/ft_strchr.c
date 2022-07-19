00ac4ce33629db147fb44baefa937eecfa0121f6
elinlim22 <hyeslim@c2r1s4.42seoul.kr>
2022-07-19 21:05:02 +0900

    strchr type trans

--- circle_1/libft/ft_strchr.c
+++ circle_1/libft/ft_strchr.c
@@ -6,17 +6,17 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 21:06:33 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/18 22:17:56 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/19 20:45:18 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s && *s != c)
+	while (*s && *s != (char)c)
 	{
 		s++;
 	}
-	if (*s == c)
+	if (*s == (char)c)
 		return ((char *)s);
 	return (0);
 }
