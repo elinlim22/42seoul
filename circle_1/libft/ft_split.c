9a8b24a261e9037e3bfdb276a4054c908b4200d2
elinlim22 <hyeslim@c4r2s5.42seoul.kr>
2022-07-20 18:02:27 +0900

    split done

--- circle_1/libft/ft_split.c
+++ circle_1/libft/ft_split.c
@@ -6,42 +6,78 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 16:10:20 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/18 14:18:12 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/20 18:01:26 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	count_row(char *s, char c)
+static int	ft_len(char *s, char c)
 {
-	size_t	count;
-	size_t	flag;
+	int	len;
+
+	len = 0;
+	while (s[len])
+	{
+		if (s[len] == c)
+			break ;
+		len++;
+	}
+	return (len);
+}
+
+static int	ft_count(char *s, char c)
+{
+	int	count;
 
 	count = 0;
-	flag = 0;
-	if (*s == 0)
-		return (0);
 	while (*s)
 	{
-		if (*s == c)
-			flag = 1;
-		if (*s != c && (flag || !*(s + 1)))
+		while (*s && *s == c)
+			s++;
+		if (*s && *s != c)
 		{
-			flag = 0;
 			count++;
 		}
-		s++;
+		while (*s && *s != c)
+			s++;
 	}
 	return (count);
 }
 
+static char	**free_all(char **s, int i)
+{
+	while (--i >= 0)
+	{
+		free(s[i]);
+	}
+	free(s);
+	return (0);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char	**result;
-	size_t	row;
+	char	**res;
+	char	*temp;
+	int		i;
 
-	row = count_row(s, c);
-	result = (char **)malloc(sizeof(char *) * (row + 1));
-	if (!result)
+	temp = (char *)s;
+	res = (char **)malloc(sizeof(char *) * (ft_count(temp, c) + 1));
+	if (!res)
 		return (NULL);
+	i = 0;
+	res[ft_count(temp, c)] = 0;
+	while (*temp)
+	{
+		while (*temp && *temp == c)
+			temp++;
+		if (*temp == 0)
+			break ;
+		res[i] = ft_substr(temp, 0, ft_len(temp, c));
+		if (!res[i])
+			return (free_all(res, i));
+		i++;
+		temp += ft_len(temp, c);
+	}
+	return (res);
 }
