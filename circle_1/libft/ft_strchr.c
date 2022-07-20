a4741a847593da46b4d3fb9a98a302b7594d1499
elinlim22 <hyeslim@c4r2s5.42seoul.kr>
2022-07-20 18:02:38 +0900

    line arrange

--- circle_1/libft/ft_strchr.c
+++ circle_1/libft/ft_strchr.c
@@ -6,16 +6,14 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 21:06:33 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/19 20:45:18 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/20 15:19:34 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s && *s != (char)c)
-	{
 		s++;
-	}
 	if (*s == (char)c)
 		return ((char *)s);
 	return (0);
