024c0cfa7efec3f718a1ab47e351a1c6be739f09
elinlim22 <hyeslim@c2r1s4.42seoul.kr>
2022-07-21 21:51:19 +0900

    null position

--- circle_1/libft/ft_strncmp.c
+++ circle_1/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 19:14:03 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/19 15:49:05 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/07/21 21:37:32 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n && (s1[i] || s2[i]))
 	{
 		if (s1[i] != s2[i])
 			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
