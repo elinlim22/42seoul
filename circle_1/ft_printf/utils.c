a9c0f3c760a369542f051ceeb8167473c241eaa6
elinlim22 <hyeslim@c8r3s10.42seoul.kr>
2022-09-22 17:19:37 +0900

    norm

--- circle_1/ft_printf/utils.c
+++ circle_1/ft_printf/utils.c
@@ -6,19 +6,19 @@
 /*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 13:50:41 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/09/22 17:12:57 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/09/22 17:18:45 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft/libft.h"
 
-int print_s(char *str)
+int	print_s(char *str)
 {
 	return (ft_putstr_fd(str, 1));
 }
 
-int print_p(void *pt)
+int	print_p(void *pt)
 {
 	unsigned long	ptr;
 	char			*hex;
@@ -50,7 +50,7 @@ void	ft_strupcase(char *str)
 	}
 }
 
-int print_xX(unsigned int i, int lu)
+int	print_xX(unsigned int i, int lu)
 {
 	char		*hex;
 	static int	res;
