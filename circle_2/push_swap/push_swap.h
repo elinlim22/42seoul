b800d01bb0f9109a2694269c7c40dc3c78b0b748
elinlim22 <hyeslim@c8r4s6.42seoul.kr>
2022-09-30 15:48:32 +0900

    nothing

--- circle_2/push_swap/push_swap.h
+++ circle_2/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 14:15:26 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/09/26 14:16:57 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/09/26 15:20:37 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PUSH_SWAP_H
 
 # include <unistd.h>
+# include <stdlib.h>
 # include "libft/libft.h"
 
 #endif
