d9fe23d3a6e3bef13e59c00c062393394ed32166
임혜서 <hyeslim@imhyeseoui-MacBookAir.local>
2022-10-05 22:13:00 +0900

    int range consideration

--- circle_2/push_swap/libft/ft_atoi.c
+++ circle_2/push_swap/libft/ft_atoi.c
@@ -6,14 +6,16 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 14:36:10 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/07/07 15:28:59 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/10/05 21:12:18 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	n;
+	long long	n;
+	int			i;
 
 	i = 1;
 	n = 0;
@@ -27,9 +29,11 @@ int	ft_atoi(const char *str)
 	}
 	while (*str >= '0' && *str <= '9')
 	{
+		if (n < -2147483648 || n > 2147483647)
+			exit(write(1, "Error\n", 6));
 		n *= 10;
 		n += *str - '0';
 		str++;
 	}
-	return (n * i);
+	return ((int)n * i);
 }
