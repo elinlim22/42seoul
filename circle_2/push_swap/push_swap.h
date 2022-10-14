ecd4d43c06b72921254a0231836e24d38ea06761
elinlim22 <elinlim22@gmail.com>
2022-10-14 21:45:08 +0900

    struct working

--- circle_2/push_swap/push_swap.h
+++ circle_2/push_swap/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 14:15:26 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/10/04 17:13:55 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/10/14 21:21:09 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,12 @@
 # include <stdlib.h>
 # include <limits.h>
 # include "libft/libft.h"
+
+int	*arg_parsing(int argc, char *argv[]);
+
+typedef	struct s_index {
+	int	idx;
+	int
+}	t_index
 
 #endif
