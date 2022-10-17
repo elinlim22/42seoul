1b101159b2db12566427839a453706eba3316f2e
임혜서 <hyeslim@imhyeseoui-MacBookAir.local>
2022-10-17 16:03:21 +0900

    functions added

--- circle_2/push_swap/push_swap.h
+++ circle_2/push_swap/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 14:15:26 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/10/14 21:21:09 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/10/17 16:01:30 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,19 @@
 # include <limits.h>
 # include "libft/libft.h"
 
-int	*arg_parsing(int argc, char *argv[]);
+char	**set_str(int argc, char *argv[]);
+int		check_first(char **res);
+int		*do_atoi(char **res, int count);
+int		check_second(int *arr);
+int		check_sorted(int *arr, int count);
+void	free_all(char **res, int count);
+int		count_args(char **res);
 
-typedef	struct s_index {
-	int	idx;
-	int
-}	t_index
+// int	*arg_parsing(int argc, char *argv[]);
+
+// typedef	struct s_index {
+// 	int	idx;
+// 	int
+// }	t_index
 
 #endif
