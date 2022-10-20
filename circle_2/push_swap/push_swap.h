b7c2bcf3603f561d6d356197f2e3b5eff3113f09
임혜서 <hyeslim@imhyeseoui-MacBookAir.local>
2022-10-20 10:49:19 +0900

    struct changed

--- circle_2/push_swap/push_swap.h
+++ circle_2/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 14:15:26 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/10/18 16:42:37 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/10/19 17:12:05 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,31 @@
 
 char	**set_str(int argc, char *argv[]);
 int		check_first(char **res);
-int		*do_atoi(char **res, int count);
-int		check_second(int *arr, int count);
-int		check_sorted(int *arr, int count);
-void	free_all(char **res, int count);
-int		count_args(char **res);
+int		*do_atoi(char **res, int size);
+int		check_second(int *arr, int size);
+int		check_sorted(int *arr, int size);
+void	free_all(char **res, int size);
+int		size_args(char **res);
 
 // int	*arg_parsing(int argc, char *argv[]);
 
-// typedef	struct s_index {
-// 	int	idx;
-// 	int
-// }	t_index
+typedef struct s_data
+{
+	int	num;
+	int	flag;
+}				t_data;
+
+typedef struct	s_stack
+{
+	t_data	*stack;
+	int	count;
+}				t_stack;
+
+typedef struct	s_all
+{
+	t_stack		*stack_a;
+	t_stack		*stack_b;
+	char		*command;
+}				t_all;
 
 #endif
