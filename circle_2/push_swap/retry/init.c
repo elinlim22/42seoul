3220d8e1d5a4175d4dca38160ae4fb04bc09004a
elinlim22 <elinlim22@gmail.com>
2022-10-25 20:26:12 +0900

    init set

--- circle_2/push_swap/retry/init.c
+++ circle_2/push_swap/retry/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 18:49:19 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/10/23 20:40:02 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/10/25 17:12:42 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,20 @@ void	init_stacks(int *arr, int count, t_stack *stacks)
 	int	i;
 
 	i = 0;
-	stacks->stack_a = (t_node *)malloc(sizeof(t_node));
+	/* stack a setting... */
+	stacks->stack_a = ft_db_lstnew(0); //head
 	if (!stacks->stack_a)
 		exit(0);
-	stacks->stack_a = ft_db_lstnew(NULL);
 	while (i < count)
 		ft_db_lstadd_back(&stacks->stack_a, ft_db_lstnew(arr[i++]));
-	stacks->stack_b = (t_node *)malloc(sizeof(t_node));
+	stacks->tail_a = ft_db_lstnew(0);
+	ft_db_lstadd_back(&stacks->stack_a, stacks->tail_a); //tail
+	/* stack b setting... */
+	stacks->stack_b = ft_db_lstnew(0); //head
 	if (!stacks->stack_b)
 		exit(0);
-	stacks->stack_b = ft_db_lstnew(NULL);
+	stacks->tail_b = ft_db_lstnew(0);
+	ft_db_lstadd_back(&stacks->stack_b, stacks->tail_b); //tail
+	/* commands setting... */
 	stacks->commands = ft_strdup("");
 }
