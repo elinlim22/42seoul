34d698ca5ea53e5363cdf2400cdf3b899d94c98e
elinlim22 <elinlim22@gmail.com>
2022-11-12 16:57:10 +0900

    norm : comments and main.c removed

--- circle_2/push_swap/libft/ft_db_lstadd_back.c
+++ circle_2/push_swap/libft/ft_db_lstadd_back.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_db_lstadd_back.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 18:50:51 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/11/03 16:28:52 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/11/12 16:55:54 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ void	ft_db_lstadd_back(t_node **where, t_node *new)
 	curr = *where;
 	if (*where == NULL)
 		*where = new;
-	else if ((*where)->type != 2) //머리에 붙이기
+	else if ((*where)->type != 2)
 	{
 		while (curr->next != NULL)
 			curr = curr->next;
 		curr->next = new;
 		new->prev = curr;
 	}
-	else //꼬리에 붙이기
+	else
 	{
 		curr->prev->next = new;
 		new->prev = curr->prev;
