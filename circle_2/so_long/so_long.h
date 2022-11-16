dbefe57b652895c37449ba0d41ae47a4843c340e
elinlim22 <elinlim22@gmail.com>
2022-11-16 21:50:31 +0900

    so_long ready

---
+++ circle_2/so_long/so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 19:11:22 by hyeslim           #+#    #+#             */
+/*   Updated: 2022/11/16 19:31:37 by hyeslim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <mlx.h>
+# include "libft/libft.h"
+
+
+
+#endif
