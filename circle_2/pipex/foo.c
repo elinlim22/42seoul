7c0ff30fc3a2cc5b525a3bd18a7a6cf69818ee8d
elinlim22 <elinlim22@gmail.com>
2022-12-10 00:17:34 +0900

    printf test

--- circle_2/pipex/foo.c
+++ circle_2/pipex/foo.c
@@ -1,23 +1,23 @@
 #include "pipex.h"
 
-int	main(int argc, char *argv[])
+int	main(void)
 {
-	t_pipex all;
+	// t_pipex all;
 
-	if (argc == 3)
-	{
-		get_path(&all);
-		// for (int i = 0; all.list_path[i]; i++)
-		// {
-		// 	printf("%s\n", all.list_path[i]);
-		// }
-		char *cmd = get_cmd(argv[2], all.list_path);
-		get_av(&all, argv[2]);
-		for (int i = 0; all.n_av[i]; i++)
-		{
-			printf("%s\n", all.n_av[i]);
-		}
-		execve(cmd, all.n_av, environ);
-	}
-	return (printf("%d\n", argc));
+	// if (argc == 3)
+	// {
+	// 	get_path(&all);
+	// 	// for (int i = 0; all.list_path[i]; i++)
+	// 	// {
+	// 	// 	printf("%s\n", all.list_path[i]);
+	// 	// }
+	// 	char *cmd = get_cmd(argv[2], all.list_path);
+	// 	get_av(&all, argv[2]);
+	// 	for (int i = 0; all.n_av[i]; i++)
+	// 	{
+	// 		printf("%s\n", all.n_av[i]);
+	// 	}
+	// 	execve(cmd, all.n_av, environ);
+	// }
+	return (ft_printf("%c\n", 'a'));
 }
