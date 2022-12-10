ec3b0f030e01426e9a78f8f8fcf7d9bf444ad8cc
elinlim22 <elinlim22@gmail.com>
2022-12-10 21:38:37 +0900

    nothing

--- circle_2/pipex/foo.c
+++ circle_2/pipex/foo.c
@@ -1,23 +1,30 @@
+// #include "pipex.h"
+
+// int	main(void)
+// {
+// 	// t_pipex all;
+
+// 	// if (argc == 3)
+// 	// {
+// 	// 	get_path(&all);
+// 	// 	// for (int i = 0; all.list_path[i]; i++)
+// 	// 	// {
+// 	// 	// 	printf("%s\n", all.list_path[i]);
+// 	// 	// }
+// 	// 	char *cmd = get_cmd(argv[2], all.list_path);
+// 	// 	get_av(&all, argv[2]);
+// 	// 	for (int i = 0; all.n_av[i]; i++)
+// 	// 	{
+// 	// 		printf("%s\n", all.n_av[i]);
+// 	// 	}
+// 	// 	execve(cmd, all.n_av, environ);
+// 	// }
+// 	return (ft_printf("%c\n", 'a'));
+// }
+
 #include "pipex.h"
 
-int	main(void)
+void	opener(t_pipex *all, char *argv, int p_case)
 {
-	// t_pipex all;
 
-	// if (argc == 3)
-	// {
-	// 	get_path(&all);
-	// 	// for (int i = 0; all.list_path[i]; i++)
-	// 	// {
-	// 	// 	printf("%s\n", all.list_path[i]);
-	// 	// }
-	// 	char *cmd = get_cmd(argv[2], all.list_path);
-	// 	get_av(&all, argv[2]);
-	// 	for (int i = 0; all.n_av[i]; i++)
-	// 	{
-	// 		printf("%s\n", all.n_av[i]);
-	// 	}
-	// 	execve(cmd, all.n_av, environ);
-	// }
-	return (ft_printf("%c\n", 'a'));
 }
