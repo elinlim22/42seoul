5bcb8d01308b6531794b7f7adea9b5ba3f880fe5
elinlim22 <elinlim22@gmail.com>
2022-12-10 20:13:38 +0900

    commentated

--- circle_2/pipex/foo/utils.c
+++ circle_2/pipex/foo/utils.c
@@ -6,40 +6,40 @@
 /*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 21:39:39 by hyeslim           #+#    #+#             */
-/*   Updated: 2022/12/07 14:45:33 by hyeslim          ###   ########.fr       */
+/*   Updated: 2022/12/10 20:13:13 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+// #include "pipex.h"
 
-void	get_path(t_pipe *pipe, char **envp)
-{
-	int		i;
+// void	get_path(t_pipe *pipe, char **envp)
+// {
+// 	int		i;
 
-	i = 0;
-	while (*envp)
-	{
-		if (ft_strnstr(*envp, "PATH", 4))
-			pipe->list_path = ft_split(*envp + 5, ':');
-		(*envp)++;
-	}
-	return ;
-}
+// 	i = 0;
+// 	while (*envp)
+// 	{
+// 		if (ft_strnstr(*envp, "PATH", 4))
+// 			pipe->list_path = ft_split(*envp + 5, ':');
+// 		(*envp)++;
+// 	}
+// 	return ;
+// }
 
-void	execve_path(t_pipe *pipe, char *cmd, char **argv, char **envp)
-{
-	char *tmp;
-	while (*(pipe->list_path))
-	{
-		tmp = *(pipe->list_path);
-		addstr(&tmp, cmd);
-		if (!access(tmp, X_OK))
-		{
-			execve(tmp, argv, envp);
-			err_msg_fd("cannot exec the command", 2);
-		}
-		else
-			*(pipe->list_path)++;
-	}
-	err_msg_fd("no sufficient PATH", 2);
-}
+// void	execve_path(t_pipe *pipe, char *cmd, char **argv, char **envp)
+// {
+// 	char *tmp;
+// 	while (*(pipe->list_path))
+// 	{
+// 		tmp = *(pipe->list_path);
+// 		addstr(&tmp, cmd);
+// 		if (!access(tmp, X_OK))
+// 		{
+// 			execve(tmp, argv, envp);
+// 			err_msg_fd("cannot exec the command", 2);
+// 		}
+// 		else
+// 			*(pipe->list_path)++;
+// 	}
+// 	err_msg_fd("no sufficient PATH", 2);
+// }
