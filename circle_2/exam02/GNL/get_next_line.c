40c78767ecc29f9ac59c68a088d3d551a3734484
elinlim22 <elinlim22@gmail.com>
2022-12-27 19:39:02 +0900

    tab arrange

--- circle_2/exam02/GNL/get_next_line.c
+++ circle_2/exam02/GNL/get_next_line.c
@@ -2,7 +2,7 @@
 
 char *get_next_line(int fd)
 {
-		char *s = malloc(10000), *c = s;
-		while (read(fd, c, 1) > 0 && *c++ != '\n');
-		return c > s ? (*c = 0, s) : (free(s), NULL);
+	char *s = malloc(10000), *c = s;
+	while (read(fd, c, 1) > 0 && *c++ != '\n');
+	return c > s ? (*c = 0, s) : (free(s), NULL);
 }
