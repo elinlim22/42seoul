50ea6c3f927accc3136809e6df5897fec9f345ba
elinlim22 <elinlim22@gmail.com>
2023-01-20 01:37:12 +0900

    philo ready

---
+++ circle_3/philosophers/philo/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 01:24:21 by hyeslim           #+#    #+#             */
+/*   Updated: 2023/01/20 01:29:35 by hyeslim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h> //pthread_detach, pthread_join, \
+						pthread_mutex_init, pthread_mutex_destroy, \
+						pthread_mutex_lock, pthread_mutex_unlock
+# include <stdlib.h> //malloc, free
+# include <stdio.h> //printf
+# include <string.h> //memset
+# include <sys/time.h> //gettimeofday
+# include <unistd.h> //write, usleep
+
+#endif
