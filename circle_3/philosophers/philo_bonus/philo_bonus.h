c2a016a25e5657ddfa3e5f58037b71805b2e8de1
elinlim22 <elinlim22@gmail.com>
2023-02-09 01:23:20 +0900

    temp

--- circle_3/philosophers/philo_bonus/philo_bonus.h
+++ circle_3/philosophers/philo_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: hyeslim <hyeslim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 01:31:28 by hyeslim           #+#    #+#             */
-/*   Updated: 2023/01/20 01:36:27 by hyeslim          ###   ########.fr       */
+/*   Updated: 2023/01/31 16:44:06 by hyeslim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 # define PHILO_BONUS_H
 
 # include <pthread.h> //pthread_create, pthread_detach, pthread_join
-# include <semaphore.h> //sem_open, sem_close, \
-						sem_post, sem_wait, sem_unlink
+# include <semaphore.h> //sem_open, sem_close,
+//						sem_post, sem_wait, sem_unlink
 # include <signal.h> //kill
 # include <stdlib.h> //malloc, free, exit
 # include <stdio.h> //printf
