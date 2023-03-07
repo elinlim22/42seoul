a6e29445446ce09f6d88595591136b9ad81d5124
elinlim22 <elinlim22@gmail.com>
2023-03-07 12:49:35 +0900

    bonus setting

---
+++ circle_3/foo2/philo_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeslim <hyeslim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 12:47:51 by hyeslim           #+#    #+#             */
+/*   Updated: 2023/03/07 12:48:02 by hyeslim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <pthread.h> //pthread_create, pthread_detach, pthread_join
+# include <semaphore.h> //sem_open, sem_close,
+//						sem_post, sem_wait, sem_unlink
+# include <signal.h> //kill
+# include <stdlib.h> //malloc, free, exit
+# include <stdio.h> //printf
+# include <string.h> //memset
+# include <sys/time.h> //gettimeofday
+# include <unistd.h> //write, usleep, fork
+
+#endif
