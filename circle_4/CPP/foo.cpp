cfc64df56c8bf828a42fc93499b1fb709ab3075b
elinlim22 <elinlim22@gmail.com>
2023-04-04 22:12:11 +0900

    practicing

--- circle_4/CPP/foo.cpp
+++ circle_4/CPP/foo.cpp
@@ -1,26 +1,59 @@
 #include <iostream>
 #include <string>
 
-// using namespace std;
+class Date {
+	private :
+		int year_;
+		int month_;
+		int day_;
 
-class Myclass
-{
-	private:
-	std::string str = "apple";
+	public :
+		void SetDate(int year, int month, int date) {
+			year_ = year;
+			month_ = month;
+			day_ = date;
+		};
+		void AddYear(int inc) {
+			year_ += inc;
+		};
+		void AddMonth(int inc) {
+			if (12 - month_ >= inc) {
+				month_ += inc;
+			} else {
+				month_ += (inc - 12);
+				year_++;
+			}
+		};
+		void AddDay(int inc) {
+			int max = 31;
+			if (month_ == 2) {
+				max = 28;
+			} else if (month_ % 2) {
+				max = 30;
+			}
+			if (max - day_ >= inc) {
+				day_ += inc;
+			} else {
+				day_ += (inc - max);
+				month_++;
+			}
+		};
 
-	public:
-	void print()
-	{
-		std::cout << str << std::endl;
-	}
+		void ShowDate() {
+			std::cout << "Year : " << year_ << std::endl;
+			std::cout << "Month : " << month_ << std::endl;
+			std::cout << "Date : " << day_ << std::endl;
+		};
 };
 
+int main() {
+	class Date date;
 
-int	main()
-{
-	Myclass class1;
+	date.SetDate(2023, 4, 4);
+	date.AddYear(3);
+	date.AddMonth(9);
+	date.AddDay(30);
 
-	class1.print();
-
+	date.ShowDate();
 	return 0;
 }
