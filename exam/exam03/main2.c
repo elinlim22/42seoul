a9e28642233ec4c483722f66e6466df3fa79060d
elinlim22 <elinlim22@gmail.com>
2023-04-11 01:04:42 +0900

    exam dir arranged

---
+++ exam/exam03/main2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct s_base {
+
+}				t_base;
+
+typedef struct shape {
+
+}				t_shape;
+
