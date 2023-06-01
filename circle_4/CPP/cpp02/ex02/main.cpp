4c37529de51405f2ab7017836da1e77028bccd26
elinlim22 <elinlim22@gmail.com>
2023-06-01 17:55:18 +0900

    main fix

--- circle_4/CPP/cpp02/ex02/main.cpp
+++ circle_4/CPP/cpp02/ex02/main.cpp
@@ -3,7 +3,6 @@
 int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	// Fixed const b(Fixed(30.5f) * Fixed(2.0f));
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
