0954b74b2fca0257a699b6977d1a8a54bf7e5c56
elinlim22 <elinlim22@gmail.com>
2023-10-06 18:10:52 +0900

    cpp08 ex01 done

--- circle_5/CPP_2/cpp08/ex01/Span.cpp
+++ circle_5/CPP_2/cpp08/ex01/Span.cpp
@@ -1,2 +1,72 @@
 #include "Span.hpp"
 
+/* -------------------------------------------------------------------------- */
+/*                                    OCCF                                    */
+/* -------------------------------------------------------------------------- */
+Span::Span() {}
+
+Span::Span(unsigned int _max) {
+	this->max = _max;
+}
+
+Span::Span(const Span& a) {
+	*this = a;
+}
+
+Span& Span::operator= (const Span& a) {
+	if (this != &a) {
+		this->max = a.max;
+		this->numbers.assign(a.numbers.begin(), a.numbers.end());
+	}
+	return *this;
+}
+
+Span::~Span() {
+
+}
+
+/* -------------------------------------------------------------------------- */
+/*                              Member functions                              */
+/* -------------------------------------------------------------------------- */
+void Span::addNumber(int n) {
+	if (this->numbers.size() == this->max) printError(FULL_NUMBERS);
+	numbers.push_back(n);
+}
+
+int Span::shortestSpan() const {
+	if (numbers.size() <= 1) printError(NOT_ENOUGH_NUMBERS);
+	std::vector<int> sortedNumbers;
+	sortedNumbers.assign(numbers.begin(), numbers.end());
+	std::sort(sortedNumbers.begin(), sortedNumbers.end());
+
+	int shortSpan = sortedNumbers[1] - sortedNumbers[0];
+	for (size_t i = 2; i < sortedNumbers.size(); i++) {
+		if (sortedNumbers[i] - sortedNumbers[i - 1] < shortSpan)
+			shortSpan = sortedNumbers[i] - sortedNumbers[i - 1];
+	}
+	return shortSpan;
+}
+
+int Span::longestSpan() const {
+	if (numbers.size() <= 1) printError(NOT_ENOUGH_NUMBERS);
+	std::vector<int> sortedNumbers;
+	sortedNumbers.assign(numbers.begin(), numbers.end());
+	std::sort(sortedNumbers.begin(), sortedNumbers.end());
+
+	int longSpan = sortedNumbers.back() - sortedNumbers.front();
+	return longSpan;
+}
+
+void Span::printError(int flag) const {
+	try {
+		switch (flag) {
+			case NOT_ENOUGH_NUMBERS:
+				throw std::runtime_error("The vector has only one number");
+			case FULL_NUMBERS:
+				throw std::runtime_error("The vector is full");
+		}
+	} catch (std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+}
--- circle_5/CPP_2/cpp08/ex01/Span.hpp
+++ circle_5/CPP_2/cpp08/ex01/Span.hpp
@@ -1,23 +1,29 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
 #include <vector>
+
+#define NOT_ENOUGH_NUMBERS 1
+#define FULL_NUMBERS 2
 
 class Span {
 	private:
-		std::vector<int> v;
-		unsigned int amount;
-		unsigned int* arr;
+		std::vector<int> numbers;
+		unsigned int max;
 		Span();
 	public:
 		Span(unsigned int max);
 		Span(const Span& a);
 		Span& operator= (const Span& a);
 		virtual ~Span();
-		void addNumber(int n); // -> if amount == max, throw std::exception
+
+		void addNumber(int n);
 		int shortestSpan() const;
 		int longestSpan() const;
-
+		void printError(int flag) const;
 };
 
 #endif
