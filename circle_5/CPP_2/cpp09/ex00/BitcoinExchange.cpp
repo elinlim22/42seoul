0e823f4e5d388c289583ae290b256ca39151837b
elinlim22 <elinlim22@gmail.com>
2023-10-12 16:52:22 +0900

    allmost there, lower_bound check remained

--- circle_5/CPP_2/cpp09/ex00/BitcoinExchange.cpp
+++ circle_5/CPP_2/cpp09/ex00/BitcoinExchange.cpp
@@ -29,14 +29,15 @@ BitcoinExchange::~BitcoinExchange() {}
 void BitcoinExchange::insertData(std::ifstream& dataBase) {
 	try {
 		std::string line;
-		std::string front;
-		float back;
+		std::string date;
+		float value;
 
+		std::getline(dataBase, line);
 		while (std::getline(dataBase, line)) {
-			splitIntoStrAndFlt(line, ",", front, back);
-			exchangeRate[front] = back;
+			splitIntoStrAndFlt(line, ",", date, value);
+			exchangeRate[date] = value;
 		}
-	} catch (std::runtime_error& e) {
+	} catch (std::runtime_error& e) { // data.csv parse error
 		std::cerr << "Error: " << e.what() << std::endl;
 		std::exit(EXIT_FAILURE);
 	}
@@ -44,24 +45,15 @@ void BitcoinExchange::insertData(std::ifstream& dataBase) {
 
 void BitcoinExchange::parseInput(std::string& str) const {
 	try {
-		std::string front;
-		float back;
+		std::string inputDate;
+		float inputPrice;
 
-		splitIntoStrAndFlt(str, " | ", front, back);
+		splitIntoStrAndFlt(str, " | ", inputDate, inputPrice);
 
-		std::map<std::string, float>::const_iterator it = exchangeRate.find(checkDate(front)); /// 안됨
-		if (it == exchangeRate.end()) {
-			// 날짜가 DB에 없으면
-			// 가장 가까운 값으로 설정
-			throw printError(3); //수정
-		}
-		printData(it, checkValue(back));
-	}
-	//  catch (std::runtime_error& e) {
-	// 	std::cerr << "Error: " << e.what() << std::endl;
-	// 	std::exit(EXIT_FAILURE);
-	// }
-	catch (std::exception& e) {
+		std::map<std::string, float>::const_iterator it = exchangeRate.lower_bound(checkDate(inputDate));
+		if (it == exchangeRate.end()) throw printError(NOKEY);
+		printData(inputDate, it->second, checkPrice(inputPrice));
+	} catch (std::exception& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 	}
 }
@@ -70,29 +62,55 @@ std::string BitcoinExchange::checkDate(std::string& str) const {
 	unsigned int year;
 	unsigned int month;
 	unsigned int day;
-	if (splitIntoYMD(str, year, month, day)) {
+	if (splitIntoYMD(str, year, month, day)) { // split and check
 		if (month == 2) {
 			if (!IsLeapYear(year) && day <= 28) return str;
 			else if (IsLeapYear(year) && day <= 29) return str;
 		} else {
-			if (day == 31 && !(month == 4 || month == 6 || month == 9 || month == 11)) return str;
+			if (day <= 31 && !(month == 4 || month == 6 || month == 9 || month == 11)) return str;
 			else if (day <= 30 && (month == 4 || month == 6 || month == 9 || month == 11)) return str;
-		} // 아닐때 else로 넘어가나?
-		return str;
+		}
 	}
-	// throw printError(WRONGKEY);
 	std::string errorString = "bad input => " + str;
 	throw std::runtime_error(errorString);
 }
 
-float BitcoinExchange::checkValue(float val) const{
+float BitcoinExchange::checkPrice(float val) const{
 	if (val < 0) throw printError(WRONGVALUE_L);
 	else if (val > 1000) throw printError(WRONGVALUE_H);
 	return val;
 }
 
-void BitcoinExchange::printData(std::map<std::string, float>::const_iterator it, float price) const {
-	std::cout << it->first << " => " << price << " = " << price * it->second << std::endl;
+void BitcoinExchange::printData(std::string& date, float value, float price) const {
+	std::cout << date << " => " << price << " = " << price * value << std::endl;
+}
+
+void BitcoinExchange::splitIntoStrAndFlt(std::string& line, const char* del, std::string& front, float& back) const {
+	size_t pos = line.find(del);
+	if (pos != std::string::npos) {
+		front = line.substr(0, pos);
+		std::istringstream iss(line.substr(pos + std::string(del).length()));
+		iss >> back;
+	} else {
+		std::string errorString = "bad input => " + line;
+		throw std::runtime_error(errorString);
+	}
+}
+
+bool BitcoinExchange::splitIntoYMD(std::string& date, unsigned int& year, unsigned int& month, unsigned int& day) const {
+	std::istringstream iss(date);
+	char c;
+	iss >> year >> c >> month >> c >> day;
+	if (year < 1 || 9999 < year || month < 1 || 12 < month || day < 1 || 31 < day) return false;
+	return true;
+}
+
+bool BitcoinExchange::IsLeapYear(int year) const {
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /* --------------------------------- Public --------------------------------- */
@@ -122,48 +140,13 @@ BitcoinExchange::printError::printError(int _flag) {
 
 const char* BitcoinExchange::printError::what() const throw() {
 	switch(flag) {
-		case WRONGKEY:
-			return ("bad input");
+		case NOKEY:
+			return ("no key has been found in DB.");
 		case WRONGVALUE_L:
 			return ("not a positive number.");
 		case WRONGVALUE_H:
 			return ("too large number.");
 		default:
 			return ("Wrong exception.");
-	}
-}
-
-/* -------------------------------------------------------------------------- */
-/*                              Utility function                              */
-/* -------------------------------------------------------------------------- */
-void splitIntoStrAndFlt(std::string& line, const char* del, std::string& front, float& back) {
-	size_t pos = line.find(del);
-	if (pos != std::string::npos) {
-		front = line.substr(0, pos);
-		std::istringstream iss(line.substr(pos + std::string(del).length()));
-		iss >> back;
-	// } else throw std::runtime_error("Parse error");
-	// } else throw BitcoinExchange::printError(WRONGKEY);
-	} else {
-		std::string errorString = "bad input => " + line;
-		throw std::runtime_error(errorString);
-	}
-
-	// } else throw BitcoinExchange::printError(WRONG)
-};
-
-bool splitIntoYMD(std::string& date, unsigned int& year, unsigned int& month, unsigned int& day) {
-	std::istringstream iss(date);
-	char c;
-	iss >> year >> c >> month >> c >> day;
-	if (year < 1 || 9999 < year || month < 1 || 12 < month || day < 1 || 31 < day) return false;
-	return true;
-}
-
-bool IsLeapYear(int year) {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-		return true;
-	} else {
-		return false;
 	}
 }
--- circle_5/CPP_2/cpp09/ex00/BitcoinExchange.hpp
+++ circle_5/CPP_2/cpp09/ex00/BitcoinExchange.hpp
@@ -7,7 +7,7 @@
 #include <sstream>
 #include <fstream>
 
-#define WRONGKEY 0
+#define NOKEY 0
 #define WRONGVALUE_L 1
 #define WRONGVALUE_H 2
 
@@ -15,20 +15,14 @@ class BitcoinExchange {
 	private:
 		std::map<std::string, float> exchangeRate;
 		// Member functions
-		bool checkFile(std::string& inputFile, const char* dataFile) const;
 		void insertData(std::ifstream& dataBase);
 		void parseInput(std::string& line) const;
 		std::string checkDate(std::string& str) const;
-		float checkValue(float val) const;
-		void printData(std::map<std::string, float>::const_iterator it, float price) const;
-	public:
-		// OCCF
-		BitcoinExchange();
-		BitcoinExchange(const BitcoinExchange& a);
-		BitcoinExchange& operator= (const BitcoinExchange& a);
-		virtual ~BitcoinExchange();
-		// Member function
-		void execute(std::string& inputFile);
+		float checkPrice(float val) const;
+		void printData(std::string& date, float value, float price) const;
+		void splitIntoStrAndFlt(std::string& line, const char* del, std::string& front, float& back) const;
+		bool splitIntoYMD(std::string& date, unsigned int& year, unsigned int& month, unsigned int& day) const;
+		bool IsLeapYear(int year) const;
 		// Exception class
 		class printError : public std::exception {
 			private:
@@ -37,14 +31,17 @@ class BitcoinExchange {
 				printError(int _flag);
 				const char* what() const throw();
 		};
+	public:
+		// OCCF
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& a);
+		BitcoinExchange& operator= (const BitcoinExchange& a);
+		virtual ~BitcoinExchange();
+		// Member function
+		void execute(std::string& inputFile);
 };
 
-void splitIntoStrAndFlt(std::string& line, const char* del, std::string& front, float& back);
-bool splitIntoYMD(std::string& date, unsigned int& year, unsigned int& month, unsigned int& day);
-bool IsLeapYear(int year);
+
 #endif
 
-// Map 사용
-// 01 - Stack 사용
-// 02 - List, Deque 사용
 
