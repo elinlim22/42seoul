a4a8ff692b6e76f3973212db64a7fbcdb1fa4ab8
elinlim22 <elinlim22@gmail.com>
2023-10-12 18:55:03 +0900

    continue

--- circle_5/CPP_2/cpp09/ex01/RPN.hpp
+++ circle_5/CPP_2/cpp09/ex01/RPN.hpp
@@ -1,4 +1,20 @@
 #ifndef RPN_HPP
 #define RPN_HPP
 
+#include <iostream>
+#include <stack>
+
+class RPN {
+	private:
+		std::stack<int> stk;
+	public:
+		// OCCF
+		RPN();
+		RPN(const RPN& a);
+		RPN& operator=(const RPN& a);
+		virtual ~RPN();
+		// Member functions
+		void calculate(std::string& line);
+};
+
 #endif
--- circle_5/CPP_2/cpp09/ex01/main.cpp
+++ circle_5/CPP_2/cpp09/ex01/main.cpp
@@ -1,5 +1,9 @@
 #include "RPN.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 2) return (static_cast<void>(std::cerr << "Wrong input" << std::endl), -1);
+	RPN rpn;
+	std::string line = std::string(argv[1]);
+	rpn.calculate(line);
 	return 0;
 }
