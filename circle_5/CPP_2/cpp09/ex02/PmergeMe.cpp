a1cb168148d393a44ca4d9ebad2e558dc05b5ec8
elinlim22 <elinlim22@gmail.com>
2023-10-14 09:44:49 +0900

    prior sorting for vector seems done

--- circle_5/CPP_2/cpp09/ex02/PmergeMe.cpp
+++ circle_5/CPP_2/cpp09/ex02/PmergeMe.cpp
@@ -25,6 +25,10 @@ PmergeMe::~PmergeMe() {
 /* -------------------------------------------------------------------------- */
 /*                              Member functions                              */
 /* -------------------------------------------------------------------------- */
+// bool comparePairs(const std::pair<int, int>& a, const std::pair<int, int>& b) {
+// 	return std::max(a.first, a.second) < std::max(b.first, b.second);
+// }
+
 void PmergeMe::initData(std::string& input) {
 	std::istringstream iss(input);
 	std::string token;
@@ -45,16 +49,28 @@ void PmergeMe::initData(std::string& input) {
 }
 
 void PmergeMe::doubling() {
-	if (originalVector.size() % 2 != 0) {
+	// 홀수인 경우 -1추가 -> 나중에 제거 필요
+	if (originalVector.size() % 2 != 0 && originalDeque.size() % 2 != 0) {
 		originalVector.push_back(-1);
 	}
 
+	// pair로 묶음
 	std::vector<std::pair<int, int> > pairs;
 	for (size_t i = 0; i < originalVector.size() - 1; i += 2) {
 		pairs.push_back(std::make_pair(originalVector[i], originalVector[i + 1]));
 	}
 
-	// std::sort(pairs.begin(), pairs.end());
+	// 앞쪽에 큰 값으로 정렬(내부)
+	for (size_t i = 0; i < originalVector.size(); ++i) {
+		if (pairs[i].first < pairs[i].second) {
+			std::swap(pairs[i].first, pairs[i].second);
+		}
+	}
+
+	// 앞을 기준으로 정렬(외부)
+	std::sort(pairs.begin(), pairs.end());
+
+	// 출력
 	for (size_t i = 0; i < pairs.size(); i++) {
 		std::cout << "[" << pairs[i].first << ", " << pairs[i].second << "] ";
 	} std::cout << std::endl;
